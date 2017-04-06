86d31694768b49d961f2eac918df1ebc7331887e
Wiseluster <chinaware@foxmail.com>
2017-04-06 14:39:03 +0800

    Update sources

--- source/neo/math/vector.d
+++ source/neo/math/vector.d
@@ -2,119 +2,119 @@ module neo.math.vector;
 
 struct Vector
 {
-	real x = 0.0L;
-	real y = 0.0L;
-	real z = 0.0L;
+    real x = 0.0L;
+    real y = 0.0L;
+    real z = 0.0L;
 
-	Vector opAdd(in Vector other) const pure nothrow @nogc @safe
-	{
-		return Vector(x + other.x, y + other.y, z + other.z);
-	}
+    Vector opAdd(in Vector other) const pure nothrow @nogc @safe
+    {
+        return Vector(x + other.x, y + other.y, z + other.z);
+    }
 
-	ref Vector opAddAssign(in Vector other) pure nothrow @nogc @safe
-	{
-		x += other.x;
-		y += other.y;
-		z += other.z;
+    ref Vector opAddAssign(in Vector other) pure nothrow @nogc @safe
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
 
-		return this;
-	}
+        return this;
+    }
 
-	Vector opSub(in Vector other) const pure nothrow @nogc @safe
-	{
-		return Vector(x - other.x, y - other.y, z - other.z);
-	}
+    Vector opSub(in Vector other) const pure nothrow @nogc @safe
+    {
+        return Vector(x - other.x, y - other.y, z - other.z);
+    }
 
-	ref Vector opSubAssign(in Vector other) pure nothrow @nogc @safe
-	{
-		x -= other.x;
-		y -= other.y;
-		z -= other.z;
+    ref Vector opSubAssign(in Vector other) pure nothrow @nogc @safe
+    {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
 
-		return this;
-	}
+        return this;
+    }
 
-	real opMul(in Vector other) const pure nothrow @nogc @safe
-	{
-		return x * other.x + y * other.y + z * other.z;
-	}
+    real opMul(in Vector other) const pure nothrow @nogc @safe
+    {
+        return x * other.x + y * other.y + z * other.z;
+    }
 
-	Vector opMul(in real scale) const pure nothrow @nogc @safe
-	{
-		return Vector(x * scale, y * scale, z * scale);
-	}
+    Vector opMul(in real scale) const pure nothrow @nogc @safe
+    {
+        return Vector(x * scale, y * scale, z * scale);
+    }
 
-	ref Vector opMulAssign(in real scale) pure nothrow @nogc @safe
-	{
-		x *= scale;
-		y *= scale;
-		z *= scale;
+    ref Vector opMulAssign(in real scale) pure nothrow @nogc @safe
+    {
+        x *= scale;
+        y *= scale;
+        z *= scale;
 
-		return this;
-	}
+        return this;
+    }
 
-	Vector opMod(in Vector other) const pure nothrow @nogc @safe
-	{
-		return Vector(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
-	}
+    Vector opMod(in Vector other) const pure nothrow @nogc @safe
+    {
+        return Vector(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+    }
 
-	ref Vector opModAssign(in Vector other) pure nothrow @nogc @safe
-	{
-		auto xn = y * other.z - z * other.y;
-		auto yn = z * other.x - x * other.z;
-		auto zn = x * other.y - y * other.x;
+    ref Vector opModAssign(in Vector other) pure nothrow @nogc @safe
+    {
+        auto xn = y * other.z - z * other.y;
+        auto yn = z * other.x - x * other.z;
+        auto zn = x * other.y - y * other.x;
 
-		x = xn;
-		y = yn;
-		z = zn;
+        x = xn;
+        y = yn;
+        z = zn;
 
-		return this;
-	}
+        return this;
+    }
 
-	Vector opPos() const pure nothrow @nogc @safe
-	{
-		return this;
-	}
+    Vector opPos() const pure nothrow @nogc @safe
+    {
+        return this;
+    }
 
-	Vector opNeg() const pure nothrow @nogc @safe
-	{
-		return Vector(-x, -y, -z);
-	}
+    Vector opNeg() const pure nothrow @nogc @safe
+    {
+        return Vector(-x, -y, -z);
+    }
 
-	real opStar() const pure nothrow @nogc @safe
-	{
-		return (x ^^ 2 + y ^^ 2 + z ^^ 2) ^^ 0.5L;
-	}
+    real opStar() const pure nothrow @nogc @safe
+    {
+        return (x ^^ 2 + y ^^ 2 + z ^^ 2) ^^ 0.5L;
+    }
 
-	string toString() const @safe
-	{
-		import std.format : format;
+    string toString() const @safe
+    {
+        import std.format : format;
 
-		return format("(%s, %s, %s)", x, y, z);
-	}
+        return format("(%s, %s, %s)", x, y, z);
+    }
 }
 
 @safe unittest
 {
-	import std.stdio : writeln;
+    import std.stdio : writeln;
 
-	immutable a = Vector(3, 4, 0.5);
-	auto b = Vector(5, -1, 2);
+    immutable a = Vector(3, 4, 0.5);
+    auto b = Vector(5, -1, 2);
 
-	writeln;
-	writeln("Unit test in " ~ __MODULE__ ~ ":");
+    writeln;
+    writeln("Unit test in " ~ __MODULE__ ~ ":");
 
-	writeln("a = ", a);
-	writeln("b = ", b);
-	writeln("a + b = ", a + b);
-	writeln("a - b = ", a - b);
-	writeln("a * b = ", a * b);
-	writeln("a * 2 = ", a * 2);
-	writeln("0.3 * a = ", 0.3 * a);
-	writeln("a % b = ", a % b);
-	writeln("+a = ", +a);
-	writeln("-a = ", -a);
-	writeln("*a = ", *a);
+    writeln("a = ", a);
+    writeln("b = ", b);
+    writeln("a + b = ", a + b);
+    writeln("a - b = ", a - b);
+    writeln("a * b = ", a * b);
+    writeln("a * 2 = ", a * 2);
+    writeln("0.3 * a = ", 0.3 * a);
+    writeln("a % b = ", a % b);
+    writeln("+a = ", +a);
+    writeln("-a = ", -a);
+    writeln("*a = ", *a);
 
-	writeln;
+    writeln;
 }
--- source/neo/time/datetime.d
+++ source/neo/time/datetime.d
@@ -2,76 +2,76 @@ module neo.time.datetime;
 
 struct DateTime
 {
-	import core.stdc.time : localtime, strftime, time, tm;
-	import std.format : FormatException, FormatSpec;
+    import core.stdc.time : localtime, strftime, time, tm;
+    import std.format : FormatException, FormatSpec;
 
-	private tm _data;
+    private tm _data;
 
-	private const void toString(void delegate(const(char)[]) sink, const(char)[] fmt)
-	{
-		import core.stdc.string : strlen;
+    private const void toString(void delegate(const(char)[]) sink, const(char)[] fmt)
+    {
+        import core.stdc.string : strlen;
 
-		auto buf = new char[256];
-		auto ptr = (fmt ~ '\0').ptr;
-		size_t len;
+        auto buf = new char[256];
+        auto ptr = (fmt ~ '\0').ptr;
+        size_t len;
 
-		foreach (n; 0 .. 4)
-		{
-			strftime(buf.ptr, buf.length, ptr, &_data);
+        foreach (n; 0 .. 4)
+        {
+            strftime(buf.ptr, buf.length, ptr, &_data);
 
-			if ((len = buf.ptr.strlen) < buf.length)
-				break;
+            if ((len = buf.ptr.strlen) < buf.length)
+                break;
 
-			buf.length *= 2;
-		}
+            buf.length *= 2;
+        }
 
-		if (sink)
-			sink(buf[0 .. len]);
-	}
+        if (sink)
+            sink(buf[0 .. len]);
+    }
 
-	const string toString()
-	{
-		string ret;
+    const string toString()
+    {
+        string ret;
 
-		toString(s => cast(void) (ret = s.idup), "%c");
+        toString(s => cast(void) (ret = s.idup), "%c");
 
-		return ret;
-	}
+        return ret;
+    }
 
-	const void toString(void delegate(const(char)[]) sink, ref FormatSpec!char fmt)
-	{
-		import std.regex : matchFirst, regex;
+    const void toString(void delegate(const(char)[]) sink, ref FormatSpec!char fmt)
+    {
+        import std.regex : matchFirst, regex;
 
-		auto str = ['%', fmt.spec];
+        auto str = ['%', fmt.spec];
 
-		if (fmt.spec == 'E' || fmt.spec == 'O')
-		{
-			str ~= fmt.trailing[0];
-			fmt.trailing = fmt.trailing[1 .. $];
-		}
+        if (fmt.spec == 'E' || fmt.spec == 'O')
+        {
+            str ~= fmt.trailing[0];
+            fmt.trailing = fmt.trailing[1 .. $];
+        }
 
-		if (str.matchFirst(`%(E?[CXYcxy]|O?[IMSUVWdemuwy]|[ABCDFGHIMRSTUVWXYZabcdeghjmnptuwxyz])`.regex).empty)
-			throw new FormatException("Incorrect format specifier " ~ str.idup ~ " for " ~ __MODULE__ ~ "." ~ DateTime.stringof);
+        if (str.matchFirst(`%(E?[CXYcxy]|O?[IMSUVWdemuwy]|[ABCDFGHIMRSTUVWXYZabcdeghjmnptuwxyz])`.regex).empty)
+            throw new FormatException("Incorrect format specifier " ~ str.idup ~ " for " ~ __MODULE__ ~ "." ~ DateTime.stringof);
 
-		toString(sink, str);
-	}
+        toString(sink, str);
+    }
 
-	pure nothrow @safe @nogc const tm toTM()
-	{
-		return _data;
-	}
+    pure nothrow @safe @nogc const tm toTM()
+    {
+        return _data;
+    }
 
-	static nothrow DateTime now()
-	{
-		auto t = time(null);
+    static nothrow DateTime now()
+    {
+        auto t = time(null);
 
-		return DateTime(*localtime(&t));
-	}
+        return DateTime(*localtime(&t));
+    }
 }
 
 unittest
 {
-	import std.stdio : writefln;
+    import std.stdio : writefln;
 
-	"%1$F %1$T".writefln(DateTime.now);
+    "%1$F %1$T".writefln(DateTime.now);
 }
